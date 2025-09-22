054447965ac7bdb4f66d3b234e6d323a2ed78044
Cassy Omondi <cksparks07@gmail.com>
2025-09-22 20:21:45 +0300

    Added links to Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,42 @@
 import { Link } from "react-router-dom";
 
-export default function Navbar() {
+function Navbar() {
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/campaigns">Campaigns</Link>
-      <Link to="/create">Create Campaign</Link>
+    <nav style={styles.nav}>
+      <h2 style={styles.logo}>FundFlow</h2>
+      <ul style={styles.links}>
+        <li><Link to="/" style={styles.link}>Home</Link></li>
+        <li><Link to="/campaigns" style={styles.link}>Campaigns</Link></li>
+        <li><Link to="/create" style={styles.link}>Create Campaign</Link></li>
+      </ul>
     </nav>
   );
 }
+
+const styles = {
+  nav: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: "1rem 2rem",
+    background: "#1976d2",
+    color: "white",
+  },
+  logo: {
+    margin: 0,
+  },
+  links: {
+    listStyle: "none",
+    display: "flex",
+    gap: "1rem",
+    margin: 0,
+    padding: 0,
+  },
+  link: {
+    color: "white",
+    textDecoration: "none",
+    fontWeight: "bold",
+  }
+};
+
+export default Navbar;
