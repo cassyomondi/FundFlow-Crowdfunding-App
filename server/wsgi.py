ed17b2495fe244977c670dfdae433d7a853ab128
Cassy Omondi <cksparks07@gmail.com>
2025-09-27 22:53:16 +0300

    Amended wsgi.py imports

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,5 +1,4 @@
-import app  # loads app.py in same directory
-app = app.app
+from .app import app  # relative import because wsgi.py and app.py are in the same package
 
 if __name__ == "__main__":
     app.run()
