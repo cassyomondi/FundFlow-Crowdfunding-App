53c062ddcc835e7ab0dc4830e0ce5b59ff24296d
Cassy Omondi <cksparks07@gmail.com>
2025-09-27 13:51:00 +0300

    Patched FLASK APP export

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
-from server.models import db, User, Campaign, Donation, bcrypt
+from models import db, User, Campaign, Donation, bcrypt
 import re
 import os
 
