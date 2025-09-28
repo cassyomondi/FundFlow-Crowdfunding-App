b92b63f888b3ce8b1adee976d6fb75d82d974576
Cassy Omondi <cksparks07@gmail.com>
2025-09-28 18:19:21 +0300

    Amended manage.py

--- manage.py
+++ manage.py
@@ -1,7 +1,11 @@
 from flask.cli import FlaskGroup
+from flask_migrate import Migrate
 from server.app import app, db
 
-# create a Flask CLI group so `flask db` commands work
+# initialize Flask-Migrate
+migrate = Migrate(app, db)
+
+# create a Flask CLI group so `flask db` commands are always available
 cli = FlaskGroup(app)
 
 if __name__ == "__main__":
