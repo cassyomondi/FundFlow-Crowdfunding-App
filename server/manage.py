deae3fcf9be14dd1daa87d3ae40838308c8e3a3a
Cassy Omondi <cksparks07@gmail.com>
2025-09-28 17:52:38 +0300

    moved  manage.py to root

--- server/manage.py
+++
@@ -1,8 +0,0 @@
-from flask.cli import FlaskGroup
-from server.app import app, db
-
-# create a Flask CLI group so `flask db` commands work
-cli = FlaskGroup(app)
-
-if __name__ == "__main__":
-    cli()
